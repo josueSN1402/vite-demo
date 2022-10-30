8ddc262bbd7004993ce0f8e9b3c8ad7118f210ce
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-10-30 16:33:29 -0500

    Import images

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 import './style.css'
 import buttonStyles from './button.module.css'
+import imageStyles from './image.module.css'
 import javascriptLogo from './javascript.svg'
+import vaiteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="${vaiteLogo}" class="${imageStyles.logo}" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${javascriptLogo}" class="${imageStyles.logo} ${imageStyles.vanilla}" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
